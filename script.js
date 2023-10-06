4a9f4c53b38035143a41484ec701898893f70aa4
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-10-06 16:45:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+	let currentlevel = document.getEelementById("level")
+function levelfinder (currentlevel)
+	{
+		let currentnode = currentlevel.parentNode;
+		let level = 1;
+       while(currentnode.tagName !== "html")
+	   {
+		currentnode.parentNode.remove()
+		currentnode = currentnode.parentNode;
+		   level++;
+       }
+		alert(`The level of the element is:${level}`);
+     }
+
+levelfinder(currentlevel);
+
